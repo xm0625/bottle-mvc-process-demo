f1c57ea327d957054c8a56ab07105adfbde3f352
xm <402276694@qq.com>
2016-09-02 11:27:33 +0800

    增加映射static目录下的所有静态文件

--- project/controller/static.py
+++ project/controller/static.py
@@ -4,11 +4,16 @@ from project.bottle import static_file
 import os
 
 
-@app.route('/:file#(favicon.ico|humans.txt)#')
+@app.route('/<file:re:(favicon.ico|humans.txt)>')
 def favicon(file):
     return static_file(file, root=os.path.join(app_root_path, 'project', 'static', 'misc'))
 
 
-@app.route('/:path#(images|css|js|fonts)\/.+#')
+@app.route('/<path:re:(images|css|js|fonts)\/.+>')
 def server_static(path):
+    return static_file(path, root=os.path.join(app_root_path, 'project', 'static'))
+
+
+@app.route('/static/<path>')
+def server_all_static(path):
     return static_file(path, root=os.path.join(app_root_path, 'project', 'static'))
